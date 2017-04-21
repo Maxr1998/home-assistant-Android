a1451fa8f98b8edace906cdbf75f79980ab5c33d
Maxr1998 <max.rumpf1998@gmail.com>
2017-04-21 13:24:25 +0200

    Cover: move utility methods to Utils

--- app/src/main/java/io/homeassistant/android/Utils.java
+++ app/src/main/java/io/homeassistant/android/Utils.java
@@ -2,7 +2,12 @@ package io.homeassistant.android;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
+import android.widget.ImageButton;
 
 import java.lang.ref.WeakReference;
 import java.util.Collections;
@@ -32,12 +37,42 @@ public final class Utils {
     }
 
     public static Set<String> getAllowedHostMismatches(Context context) {
-        return getPrefs(context).getStringSet(Common.PREF_ALLOWED_HOST_MISMATCHES_KEY, Collections.<String>emptySet());
+        return getPrefs(context).getStringSet(Common.PREF_ALLOWED_HOST_MISMATCHES_KEY, Collections.emptySet());
     }
 
     public static void addAllowedHostMismatch(Context context, String allowed) {
         Set<String> set = new HashSet<>(getAllowedHostMismatches(context));
         set.add(allowed);
         getPrefs(context).edit().putStringSet(Common.PREF_ALLOWED_HOST_MISMATCHES_KEY, set).apply();
+    }
+
+    /**
+     * Sets the specified image button to the given state, while modifying or "graying-out"
+     * the icon as well
+     *
+     * @param enabled   The state of the menu item
+     * @param item      The menu item to modify
+     * @param iconResId The icon ID
+     */
+    public static void setStatefulImageButtonIcon(Context c, boolean enabled, ImageButton item, int iconResId) {
+        item.setEnabled(enabled);
+        Drawable originalIcon = ContextCompat.getDrawable(c, iconResId);
+        Drawable icon = enabled ? originalIcon : convertDrawableToGrayScale(originalIcon);
+        item.setImageDrawable(icon);
+    }
+
+    /**
+     * Mutates and applies a filter that converts the given drawable to a Gray image.
+     * This method may be used to simulate the color of disabled icons in Honeycomb's ActionBar.
+     *
+     * @return a mutated version of the given drawable with a color filter applied.
+     */
+    private static Drawable convertDrawableToGrayScale(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+        Drawable res = drawable.mutate();
+        res.setColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_IN);
+        return res;
     }
 }
--- app/src/main/java/io/homeassistant/android/view/viewholders/CoverViewHolder.java
+++ app/src/main/java/io/homeassistant/android/view/viewholders/CoverViewHolder.java
@@ -1,16 +1,14 @@
 package io.homeassistant.android.view.viewholders;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ImageButton;
+
 import io.homeassistant.android.HassActivity;
 import io.homeassistant.android.R;
 import io.homeassistant.android.api.requests.ToggleRequest;
 import io.homeassistant.android.api.results.Entity;
+
+import static io.homeassistant.android.Utils.setStatefulImageButtonIcon;
 
 public class CoverViewHolder extends TextViewHolder implements View.OnClickListener {
 
@@ -31,41 +29,8 @@ public class CoverViewHolder extends TextViewHolder implements View.OnClickListe
     @Override
     public void setEntity(Entity e) {
         super.setEntity(e);
-        setImageButtonEnabled(itemView.getContext(), e.state.equalsIgnoreCase("open"), buttonCoverDown, R.drawable.ic_arrow_downward_24dp);
-        setImageButtonEnabled(itemView.getContext(), e.state.equalsIgnoreCase("closed"), buttonCoverUp, R.drawable.ic_arrow_upward_24dp);
-    }
-
-    /**
-     * Sets the specified image button to the given state, while modifying or
-     * "graying-out" the icon as well
-     *
-     * @param enabled The state of the menu item
-     * @param item The menu item to modify
-     * @param iconResId The icon ID
-     */
-    public static void setImageButtonEnabled(Context ctxt, boolean enabled, ImageButton item,
-                                             int iconResId) {
-        item.setEnabled(enabled);
-        Drawable originalIcon = ContextCompat.getDrawable(ctxt, iconResId);
-        Drawable icon = enabled ? originalIcon : convertDrawableToGrayScale(originalIcon);
-        item.setImageDrawable(icon);
-    }
-
-    /**
-     * Mutates and applies a filter that converts the given drawable to a Gray
-     * image. This method may be used to simulate the color of disable icons in
-     * Honeycomb's ActionBar.
-     *
-     * @return a mutated version of the given drawable with a color filter
-     *         applied.
-     */
-    public static Drawable convertDrawableToGrayScale(Drawable drawable) {
-        if (drawable == null) {
-            return null;
-        }
-        Drawable res = drawable.mutate();
-        res.setColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_IN);
-        return res;
+        setStatefulImageButtonIcon(itemView.getContext(), e.state.equalsIgnoreCase("open"), buttonCoverDown, R.drawable.ic_arrow_downward_24dp);
+        setStatefulImageButtonIcon(itemView.getContext(), e.state.equalsIgnoreCase("closed"), buttonCoverUp, R.drawable.ic_arrow_upward_24dp);
     }
 
     @Override
