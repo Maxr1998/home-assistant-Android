9981e3ccff6983fe1679b4f3b984e95cd10aa2cc
Maxr1998 <max.rumpf1998@gmail.com>
2017-05-04 18:20:54 +0200

    Reformat the event classes

--- app/src/main/java/io/homeassistant/android/api/requests/SubscribeEventsRequest.java
+++ app/src/main/java/io/homeassistant/android/api/requests/SubscribeEventsRequest.java
@@ -3,11 +3,10 @@ package io.homeassistant.android.api.requests;
 import com.afollestad.ason.Ason;
 
 public class SubscribeEventsRequest extends Ason {
-
     protected final String type = "subscribe_events";
-    protected String event_type = null;
+    protected final String event_type;
 
-    public SubscribeEventsRequest(String eventType) {
-        this.event_type = eventType;
+    public SubscribeEventsRequest(String e) {
+        event_type = e;
     }
 }
--- app/src/main/java/io/homeassistant/android/api/results/EventData.java
+++ app/src/main/java/io/homeassistant/android/api/results/EventData.java
@@ -1,7 +1,6 @@
 package io.homeassistant.android.api.results;
 
 public class EventData {
-
     public String entity_id;
     public Entity new_state;
     public Entity old_state;
